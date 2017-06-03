1506d5433b1d4d11754958db7fd6bb44d287fc61
Adrián Obiol <theadobis1@gmail.com>
2017-06-03 14:18:20 +0200

    Mejora de la web y comentarios

---
+++ src/main/webapp/app/pages/lore.state.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('letItJumpApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('prueba', {
+            parent: 'app',
+            url: '/lore',
+            data: {
+                authorities: []
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/pages/lore.html'
+                }
+            },
+            resolve: {
+                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('lore');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
